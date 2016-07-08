25a136f6b38642cc4207af25d31d8eaef55dd4e9
LvBingru <lvbingru@gmail.com>
2016-07-08 15:03:56 +0800

    android gnu image update

--- android/src/main/java/com/facebook/react/views/image/RCTGPUImageView.java
+++ android/src/main/java/com/facebook/react/views/image/RCTGPUImageView.java
@@ -1,38 +1,93 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
 package com.facebook.react.views.image;
 
-import android.content.Context;
+import javax.annotation.Nullable;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+
+import com.facebook.common.executors.UiThreadImmediateExecutorService;
+import com.facebook.common.references.CloseableReference;
+import com.facebook.common.util.UriUtil;
+import com.facebook.csslayout.CSSConstants;
+import com.facebook.csslayout.FloatUtil;
+import com.facebook.datasource.DataSource;
+import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.controller.AbstractDraweeControllerBuilder;
+import com.facebook.drawee.controller.BaseControllerListener;
+import com.facebook.drawee.controller.ControllerListener;
+import com.facebook.drawee.controller.ForwardingControllerListener;
+import com.facebook.drawee.drawable.AutoRotateDrawable;
+import com.facebook.drawee.drawable.ScalingUtils;
+import com.facebook.drawee.generic.GenericDraweeHierarchy;
+import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder;
+import com.facebook.drawee.generic.RoundingParams;
+import com.facebook.drawee.view.GenericDraweeView;
+import com.facebook.imagepipeline.common.ResizeOptions;
+import com.facebook.imagepipeline.core.ImagePipeline;
+import com.facebook.imagepipeline.datasource.BaseBitmapDataSubscriber;
+import com.facebook.imagepipeline.image.CloseableImage;
+import com.facebook.imagepipeline.image.ImageInfo;
+import com.facebook.imagepipeline.request.BasePostprocessor;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
+import com.facebook.imagepipeline.request.Postprocessor;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableMapKeySetIterator;
 import com.facebook.react.bridge.ReadableType;
+import com.facebook.react.common.SystemClock;
+import com.facebook.react.uimanager.PixelUtil;
+import com.facebook.react.uimanager.UIManagerModule;
+import com.facebook.react.uimanager.events.EventDispatcher;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 
-import javax.annotation.Nullable;
-
+import jp.co.cyberagent.android.gpuimage.GPUImage;
 import jp.co.cyberagent.android.gpuimage.GPUImageFilter;
 import jp.co.cyberagent.android.gpuimage.GPUImageFilterGroup;
 import jp.co.cyberagent.android.gpuimage.GPUImageView;
 
 /**
- * Created by lvbingru on 6/24/16.
+ * Wrapper class around Fresco's GenericDraweeView, enabling persisting props across multiple view
+ * update and consistent processing of both static and network images.
  */
-public class RCTGPUImageView extends ReactImageView {
-    public RCTGPUImageView(Context context, AbstractDraweeControllerBuilder draweeControllerBuilder, @Nullable Object callerContext) {
-        super(context, draweeControllerBuilder, callerContext);
+public class RCTGPUImageView extends GPUImageView {
 
-        mGPUImageView = new GPUImageView(context);
+    private GPUImageFilterGroup mFilterGroup;
 
+    /**
+     * Instantiates a new GPUImage object.
+     *
+     * @param context the context
+     */
+    public RCTGPUImageView(Context context) {
+        super(context);
     }
 
-    ReadableArray mFilters;
-    GPUImageFilterGroup mFilterGroup;
-    GPUImageView mGPUImageView;
-
     public void setFilters(ReadableArray filters) {
-        mFilters = filters;
         boolean needUpdate = false;
         int count = filters.size();
         if (mFilterGroup==null || mFilterGroup.getFilters().size() != count) {
@@ -75,6 +130,7 @@ public class RCTGPUImageView extends ReactImageView {
                     }
                 }
             }
+            this.setFilter(mFilterGroup);
         }
 
         for (int i = 0 ; i<count ; i++) {
@@ -99,13 +155,74 @@ public class RCTGPUImageView extends ReactImageView {
                 }
             }
         }
-        this.updateGPUImage();
+
     }
 
-    private void updateGPUImage() {
-        if (mFilterGroup!=null) {
-            mGPUImageView.setFilter(mFilterGroup);
-            mGPUImageView.requestRender();
+    private Bitmap savedBitmap;
+    private String lastImageUri;
+
+    public void setSource(final String imageUri){
+//        this.setImage(Uri.parse(imageUri));
+        lastImageUri = imageUri;
+        if (imageUri != null) {
+            _getImage(Uri.parse(imageUri), null, new ImageCallback() {
+                @Override
+                public void invoke(@Nullable Bitmap bitmap) {
+                    if (imageUri.equals(lastImageUri)) {
+                        savedBitmap = bitmap;
+                        setImage(bitmap);
+                    }
+                }
+            });
+        }
+    }
+
+    private interface ImageCallback {
+        void invoke(@Nullable Bitmap bitmap);
+    }
+
+    private void _getImage(Uri uri, ResizeOptions resizeOptions, final ImageCallback imageCallback) {
+        BaseBitmapDataSubscriber dataSubscriber = new BaseBitmapDataSubscriber() {
+            @Override
+            protected void onNewResultImpl(Bitmap bitmap) {
+                bitmap = bitmap.copy(bitmap.getConfig(), true);
+                imageCallback.invoke(bitmap);
+            }
+
+            @Override
+            protected void onFailureImpl(DataSource<CloseableReference<CloseableImage>> dataSource) {
+                imageCallback.invoke(null);
+            }
+        };
+
+        ImageRequestBuilder builder = ImageRequestBuilder.newBuilderWithSource(uri);
+        if (resizeOptions != null) {
+            builder = builder.setResizeOptions(resizeOptions);
+        }
+        ImageRequest imageRequest = builder.build();
+
+        ImagePipeline imagePipeline = Fresco.getImagePipeline();
+        DataSource<CloseableReference<CloseableImage>> dataSource = imagePipeline.fetchDecodedImage(imageRequest, null);
+        dataSource.subscribe(dataSubscriber, UiThreadImmediateExecutorService.getInstance());
+    }
+
+    private static Uri getResourceDrawableUri(Context context, String name) {
+        if (name == null || name.isEmpty()) {
+            return null;
+        }
+        name = name.toLowerCase().replace("-", "_");
+        int resId = context.getResources().getIdentifier(
+                name,
+                "drawable",
+                context.getPackageName());
+
+        if (resId == 0) {
+            return null;
+        } else {
+            return new Uri.Builder()
+                    .scheme(UriUtil.LOCAL_RESOURCE_SCHEME)
+                    .path(String.valueOf(resId))
+                    .build();
         }
     }
 }
--- android/src/main/java/com/facebook/react/views/image/RCTGPUImageViewManager.java
+++ android/src/main/java/com/facebook/react/views/image/RCTGPUImageViewManager.java
@@ -28,20 +28,8 @@ public class RCTGPUImageViewManager extends SimpleViewManager<RCTGPUImageView> {
 
     private ResourceDrawableIdHelper mResourceDrawableIdHelper;
     private @Nullable AbstractDraweeControllerBuilder mDraweeControllerBuilder;
-    private final @Nullable Object mCallerContext;
-
-    public RCTGPUImageViewManager(
-            AbstractDraweeControllerBuilder draweeControllerBuilder,
-            Object callerContext) {
-        mDraweeControllerBuilder = draweeControllerBuilder;
-        mCallerContext = callerContext;
-        mResourceDrawableIdHelper = new ResourceDrawableIdHelper();
-    }
 
     public RCTGPUImageViewManager() {
-        // Lazily initialize as FrescoModule have not been initialized yet
-        mDraweeControllerBuilder = null;
-        mCallerContext = null;
         mResourceDrawableIdHelper = new ResourceDrawableIdHelper();
     }
 
@@ -52,22 +40,16 @@ public class RCTGPUImageViewManager extends SimpleViewManager<RCTGPUImageView> {
         return mDraweeControllerBuilder;
     }
 
-    public Object getCallerContext() {
-        return mCallerContext;
-    }
 
     @Override
     public String getName() {
         return REACT_CLASS;
     }
 
-
     @Override
     public RCTGPUImageView createViewInstance(ThemedReactContext context) {
         return new RCTGPUImageView(
-                context,
-                getDraweeControllerBuilder(),
-                getCallerContext());
+                context);
     }
 
 
@@ -79,85 +61,9 @@ public class RCTGPUImageViewManager extends SimpleViewManager<RCTGPUImageView> {
     // In JS this is Image.props.source.uri
     @ReactProp(name = "src")
     public void setSource(RCTGPUImageView view, @Nullable String source) {
-        view.setSource(source, mResourceDrawableIdHelper);
+        view.setSource(source);
     }
 
-    // In JS this is Image.props.loadingIndicatorSource.uri
-    @ReactProp(name = "loadingIndicatorSrc")
-    public void setLoadingIndicatorSource(RCTGPUImageView view, @Nullable String source) {
-        view.setLoadingIndicatorSource(source, mResourceDrawableIdHelper);
-    }
-
-    @ReactProp(name = "borderColor", customType = "Color")
-    public void setBorderColor(RCTGPUImageView view, @Nullable Integer borderColor) {
-        if (borderColor == null) {
-            view.setBorderColor(Color.TRANSPARENT);
-        } else {
-            view.setBorderColor(borderColor);
-        }
-    }
-
-    @ReactProp(name = "overlayColor")
-    public void setOverlayColor(RCTGPUImageView view, @Nullable Integer overlayColor) {
-        if (overlayColor == null) {
-            view.setOverlayColor(Color.TRANSPARENT);
-        } else {
-            view.setOverlayColor(overlayColor);
-        }
-    }
-
-    @ReactProp(name = "borderWidth")
-    public void setBorderWidth(RCTGPUImageView view, float borderWidth) {
-        view.setBorderWidth(borderWidth);
-    }
-
-    @ReactPropGroup(names = {
-            ViewProps.BORDER_RADIUS,
-            ViewProps.BORDER_TOP_LEFT_RADIUS,
-            ViewProps.BORDER_TOP_RIGHT_RADIUS,
-            ViewProps.BORDER_BOTTOM_RIGHT_RADIUS,
-            ViewProps.BORDER_BOTTOM_LEFT_RADIUS
-    }, defaultFloat = CSSConstants.UNDEFINED)
-    public void setBorderRadius(RCTGPUImageView view, int index, float borderRadius) {
-        if (!CSSConstants.isUndefined(borderRadius)) {
-            borderRadius = PixelUtil.toPixelFromDIP(borderRadius);
-        }
-
-        if (index == 0) {
-            view.setBorderRadius(borderRadius);
-        } else {
-            view.setBorderRadius(borderRadius, index - 1);
-        }
-    }
-
-    @ReactProp(name = ViewProps.RESIZE_MODE)
-    public void setResizeMode(RCTGPUImageView view, @Nullable String resizeMode) {
-        view.setScaleType(ImageResizeMode.toScaleType(resizeMode));
-    }
-
-    @ReactProp(name = "tintColor", customType = "Color")
-    public void setTintColor(RCTGPUImageView view, @Nullable Integer tintColor) {
-        if (tintColor == null) {
-            view.clearColorFilter();
-        } else {
-            view.setColorFilter(tintColor, PorterDuff.Mode.SRC_IN);
-        }
-    }
-
-    @ReactProp(name = "progressiveRenderingEnabled")
-    public void setProgressiveRenderingEnabled(RCTGPUImageView view, boolean enabled) {
-        view.setProgressiveRenderingEnabled(enabled);
-    }
-
-    @ReactProp(name = "fadeDuration")
-    public void setFadeDuration(RCTGPUImageView view, int durationMs) {
-        view.setFadeDuration(durationMs);
-    }
-
-    @ReactProp(name = "shouldNotifyLoadEvents")
-    public void setLoadHandlersRegistered(RCTGPUImageView view, boolean shouldNotifyLoadEvents) {
-        view.setShouldNotifyLoadEvents(shouldNotifyLoadEvents);
-    }
 
     @Override
     public @Nullable Map getExportedCustomDirectEventTypeConstants() {
@@ -171,9 +77,4 @@ public class RCTGPUImageViewManager extends SimpleViewManager<RCTGPUImageView> {
         );
     }
 
-    @Override
-    protected void onAfterUpdateTransaction(RCTGPUImageView view) {
-        super.onAfterUpdateTransaction(view);
-        view.maybeUpdateView();
-    }
 }
