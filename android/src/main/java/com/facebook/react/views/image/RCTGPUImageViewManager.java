279188ebe348bf789369933d0af5b8d0b3f899f9
LvBingru <lvbingru@gmail.com>
2016-08-06 23:21:57 +0800

    bug fixed

--- android/src/main/java/com/facebook/react/views/image/RCTGPUImageViewManager.java
+++ android/src/main/java/com/facebook/react/views/image/RCTGPUImageViewManager.java
@@ -1,19 +1,12 @@
 package com.facebook.react.views.image;
 
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-
-import com.facebook.csslayout.CSSConstants;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.controller.AbstractDraweeControllerBuilder;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.common.MapBuilder;
-import com.facebook.react.uimanager.PixelUtil;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
-import com.facebook.react.uimanager.ViewProps;
 import com.facebook.react.uimanager.annotations.ReactProp;
-import com.facebook.react.uimanager.annotations.ReactPropGroup;
 
 import java.util.Map;
 
@@ -26,11 +19,9 @@ public class RCTGPUImageViewManager extends SimpleViewManager<RCTGPUImageView> {
 
     public static final String REACT_CLASS = "RCTGPUImageView";
 
-    private ResourceDrawableIdHelper mResourceDrawableIdHelper;
     private @Nullable AbstractDraweeControllerBuilder mDraweeControllerBuilder;
 
     public RCTGPUImageViewManager() {
-        mResourceDrawableIdHelper = new ResourceDrawableIdHelper();
     }
 
     public AbstractDraweeControllerBuilder getDraweeControllerBuilder() {
